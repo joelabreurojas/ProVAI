00a57dd38c9f58743f40c930728b95c6041637ec
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-08-18 15:27:17 -0400

    feat(ai): add custom exceptions for model handling

---
+++ src/ai/application/exceptions/__init__.py
@@ -0,0 +1,9 @@
+from .model_configuration_error import ModelConfigurationError
+from .model_load_error import ModelLoadError
+from .model_not_found_error import ModelNotFoundError
+
+__all__ = [
+    "ModelConfigurationError",
+    "ModelLoadError",
+    "ModelNotFoundError",
+]
